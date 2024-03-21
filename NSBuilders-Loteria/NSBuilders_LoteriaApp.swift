81f44396d80d865ffe7cfa897cb69c3559ae9659
Lorenzo Brown <lrnzbr@gmail.com>
2024-03-20 20:54:20 -0600

    Checkpoint 1
    commit inicial

---
+++ NSBuilders-Loteria/NSBuilders_LoteriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NSBuilders_LoteriaApp.swift
+//  NSBuilders-Loteria
+//
+//  Created by Lorenzo Brown on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct NSBuilders_LoteriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
