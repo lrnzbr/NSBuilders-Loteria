39bcfe7b9e65f9e78d729663ac10b032d7809c20
Lorenzo Brown <lrnzbr@gmail.com>
2024-03-20 21:20:28 -0600

    Checkpoint 3 - GridView

---
+++ NSBuilders-Loteria/GridView.swift
@@ -0,0 +1,38 @@
+//
+//  GridView.swift
+//  NSBuilders-Loteria
+//
+//  Created by Lorenzo Brown on 3/20/24.
+//
+
+import SwiftUI
+
+struct GridView: View {
+    
+    let columns: [GridItem] = [
+        GridItem(.flexible(), spacing: 12),
+        GridItem(.flexible(), spacing: 12),
+        GridItem(.flexible(), spacing: 12),
+        GridItem(.flexible(), spacing: 12)
+    ]
+    
+    var body: some View {
+        GeometryReader { geo in
+            ZStack {
+                LazyVGrid(columns: columns)
+                {
+                    ForEach(0...15, id: \.self) { number in
+                        CellView()
+                            .frame(maxWidth: geo.size.width / 4, maxHeight: geo.size.height / 4)
+                    }
+                }
+            }.padding(.bottom)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+        }
+    }
+}
+
+#Preview {
+    GridView()
+}
